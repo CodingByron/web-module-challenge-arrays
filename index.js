59c2835ce3adaafb291b3f79e3339472f74d397d
Byron Sanders <byrone_sanders@hotmail.com>
2022-11-02 10:10:37 -0700

    answered task 2

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ Use the copy function below to do the following:
 */
 
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(originalFlavors){
+  
 }
 
 
@@ -63,9 +63,14 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-  /*your code here*/
+function is31Flavors(originalFlavors){
+  if(originalFlavors.length === 31){
+    console.log(true);
+  } else{
+    console.log(false);
+  }
  }
+ is31Flavors(originalFlavors);
 
 
 
